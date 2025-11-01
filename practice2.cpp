1f37a2892c28ef78fb96cacf74773c3495d43811
archanapoojary <achhupujari@gmail.com>
2025-11-01 23:45:05 +0530

    4th commit

---
+++ practice2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    int b;
+    cout << "enter a num ";
+    cin >> a;
+    cout << "enter second number";
+    cin >> b;
+    if (a>b ){
+        cout << a << " is greater";
+    }
+    else {
+        cout << b << "is greater";
+    }
+    return 0;
+    }
